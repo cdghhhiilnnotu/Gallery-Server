076e8c4b8c99faae830a2db4782dbc345f86d636
cdghhhiilnnotu <nguyenthanhduong22072002@gmail.com>
2024-06-17 02:58:50 +0700

    compress image 12 107

---
+++ compress_image.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+def compress_image_to_jpeg(input_image_path, output_image_path, quality=85):
+    # Open the image file
+    with Image.open(input_image_path) as img:
+        # Convert the image to RGB mode if it's not already in that mode
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        # Save the image with the specified quality level
+        img.save(output_image_path, 'JPEG', quality=quality)
+
+# Example usage:
+compress_image_to_jpeg('src\images\i0000000012.png', 'src\images\i0000000012.png', quality=1)
+compress_image_to_jpeg('src\images\i0000000107.jpg', 'src\images\i0000000107.jpg', quality=80)
